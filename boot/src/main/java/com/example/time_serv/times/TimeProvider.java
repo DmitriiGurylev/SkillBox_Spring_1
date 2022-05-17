f4950b11d43670afcc56c403624ab5dc17c18436
Wonderful-guy <gurylev.dmitrii@mail.ru>
2022-05-17 17:52:13 +0300

    boot module finished

--- boot/src/main/java/com/example/time_serv/times/TimeProvider.java
+++ boot/src/main/java/com/example/time_serv/times/TimeProvider.java
@@ -1,5 +1,7 @@
 package com.example.time_serv.times;
 
+import com.example.external.TimeService;
+import com.example.external.TimeServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -8,9 +10,10 @@ import org.springframework.stereotype.Component;
 import java.util.logging.Logger;
 
 @Component
+//@Profile("prod")
 public class TimeProvider implements CommandLineRunner {
 
-    private final TimeService timeService;
+    private final TimeServiceInterface timeService;
     private final TimeProviderProperties timeProviderProperties;
 
     @Value("${spring.application.name}")
@@ -20,7 +23,7 @@ public class TimeProvider implements CommandLineRunner {
     private String welcomeMessage;
 
     @Autowired
-    public TimeProvider(TimeService timeService, TimeProviderProperties timeProviderProperties) {
+    public TimeProvider(TimeServiceInterface timeService, TimeProviderProperties timeProviderProperties) {
         this.timeService = timeService;
         this.timeProviderProperties = timeProviderProperties;
     }
