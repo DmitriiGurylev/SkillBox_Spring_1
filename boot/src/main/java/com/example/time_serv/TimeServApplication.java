c817886b3ebff00ef922a2706cd68fcd1e8913bc
Wonderful-guy <gurylev.dmitrii@mail.ru>
2022-05-17 16:15:58 +0300

    TimeProvider and TimeService added

--- boot/src/main/java/com/example/time_serv/TimeServApplication.java
+++ boot/src/main/java/com/example/time_serv/TimeServApplication.java
@@ -1,8 +1,14 @@
 package com.example.time_serv;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+//@ComponentScan
+//@EnableAutoConfiguration
+//@SpringBootConfiguration
 @SpringBootApplication
 public class TimeServApplication {
 
